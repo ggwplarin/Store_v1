8c9de7428edb0efb628156108984c26baa5be46b
ggwplarin <al8137962@gmail.com>
2020-10-07 12:45:34 +0300

    f

---
+++ Store_v1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Store_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
